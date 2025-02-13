254c8873315df413f0dec73a6bade71905a001d7
MrPuxe <anthondeleonsketch@gmail.com>
2025-02-12 20:05:49 -0500

    added the constructors needed

--- ComboLock.java
+++ ComboLock.java
@@ -1,17 +1,31 @@
 //Initialize class 
+public class ComboLock{
 
     //Delcare private instance variables
 
     //Delcare: correctCombination
-    //Delcare: currentCOmbination
+    private String correctCombination;
+    //Delcare: currentCombination
+    private String currentCombination;
     //Declare: isOpen
+    private boolean isOpen;
 
     //Initialize constructor with no arguments
+    public ComboLock()
+    {
+        this.correctCombination = "123";
+        this.currentCombination = "123";
+        this.isOpen = true;
+    }
 
     //Initializ constructor with a passing string argument 
+    public ComboLock(String combination){
         //Set the passing argument to be the combination for this class
+        this.correctCombination = combination;
+        this.currentCombination = combination;
         //default open the lock
-
+        this.isOpen = true;
+    }
     //Initialize the toString() method which will return the state of this class lock
 
 
@@ -30,3 +44,5 @@
     //Lets have a method that gives us the difference between the two locks current and correct combination
 
     //Lets have a method that checks which lock is closser to getting open, that will required our previus method to check the difference between two locks
+
+}
