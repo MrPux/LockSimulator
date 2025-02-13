eee236f221773d2a86e14d3655b46b60c15899ee
MrPuxe <anthondeleonsketch@gmail.com>
2025-02-12 19:25:03 -0500

    Added a pseudocode for my main to test our the methods that I will build for ComboLock class.

---
+++ main.java
@@ -0,0 +1,22 @@
+// Class main
+
+    //An instance object of a comboLock without lock combination passing arguments
+
+    //I will also test an instance object of comboLock with passing arguments that would be the lock combinations
+
+    //test the to instanceObject.toString() method
+
+    //test a .close() method to shut the lock
+
+    //test a .setValueNum method to unlock the lock
+
+    //test a .open() method to open the lock
+
+
+    //Test which out my two locks I created is closer to being solved.
+
+    //Test if an open & closed methods have an equal combination 
+
+    //Test if an open & open methods have equals combination
+
+
